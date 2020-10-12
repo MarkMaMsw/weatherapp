6943cb4be6a7523ba53b1b566a3da5c67ae8d15b
Mark <markmamsw@gmail.com>
2020-10-12 18:09:17 +1100

    add Header and Main components

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FcMenu } from "react-icons/fc";
+
+const Header = () => {
+    return <header><FcMenu/></header>;
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Main = () => {
+    return <main>
+        <div className="card">
+            <div className="card__main">
+                {/* <CityWeather/> */}
+            </div>
+            <div className="card__details">
+                <div className="card__details_left">
+                    {/* <Twitter/> */}
+                </div>
+                <div className="card__details_right">
+                    {/* <ForcastList/> */}
+                </div>
+            </div>
+        </div>
+    </main>;
+}
+
+export default Main;
